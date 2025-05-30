d26aa8996259c579152b635ed64cdb630233c024
ENTAR0 <img1518@gmail.com>
2025-05-30 11:16:02 +0900

    Front Folder Tree

---
+++ frontend/aivle_04/src/components/BookCard.jsx
@@ -0,0 +1,13 @@
+// src/components/BookCard.jsx
+import React from 'react';
+
+const BookCard = () => {
+  return (
+    <div className="book-card">
+      <div className="book-thumbnail" />
+      <div className="book-title">책제목</div>
+    </div>
+  );
+};
+
+export default BookCard;
---
+++ frontend/aivle_04/src/components/Header.jsx
@@ -0,0 +1,17 @@
+// src/components/Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top-menu">
+        <a href="/help">help</a>
+        <a href="/register">Register</a>
+        <a href="/login">login</a>
+      </div>
+      <div className="logo">Logo</div>
+    </header>
+  );
+};
+
+export default Header;
