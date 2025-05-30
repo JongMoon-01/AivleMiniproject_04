da4858c551768b4f3e7b4ef4366eff78afcf0d19
AI-SW-Safety-Guard <swh8775@gmail.com>
2025-05-30 14:50:15 +0900

    로그인 테스트트

--- frontend/aivle_04/src/App.js
+++ frontend/aivle_04/src/App.js
@@ -1,9 +1,19 @@
-// src/App.jsx
 import React from 'react';
-import AppRouter from './routes/Router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import MainPage from './pages/MainPage'; 
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
