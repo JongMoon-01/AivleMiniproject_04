051eb1d34cc6a578218796904f88f668c027f395
Jun-gi Kim <damnum3@hanyang.ac.kr>
2025-06-01 21:27:01 +0900

    updateBook 수정

--- backend/book/src/main/java/com/Aivleminiproject_04/book/service/BookServiceImpl.java
+++ backend/book/src/main/java/com/Aivleminiproject_04/book/service/BookServiceImpl.java
@@ -9,6 +9,7 @@ import com.Aivleminiproject_04.book.repository.BookRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,13 +52,13 @@ public class BookServiceImpl implements BookService {
                 .orElseThrow();
 
         // DTO의 필드가 null이 아닐 경우에만 업데이트 (즉 부분 업데이트 지원)
-        if (requestDto.getTitle() != null)
+        if (StringUtils.hasText(requestDto.getTitle()))
             book.setTitle(requestDto.getTitle());
-        if (requestDto.getContent() != null)
+        if (StringUtils.hasText(requestDto.getContent()))
             book.setContent(requestDto.getContent());
-        if (requestDto.getTag() != null)
+        if (StringUtils.hasText(requestDto.getTag()))
             book.setTag(requestDto.getTag());
-        if (requestDto.getCoverImageUrl() != null)
+        if (StringUtils.hasText(requestDto.getCoverImageUrl()))
             book.setCoverImageUrl(requestDto.getCoverImageUrl());
 
         return new BookResponseDto(bookRepository.save(book));
